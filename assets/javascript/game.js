95b0c105b3c61a578b6184313b79b34b3d4c7dd7
rwon@corp.drillmap.com <rwon@corp.drillmap.com>
2018-08-16 11:39:24 -0500

    add game.js, finish the logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,84 @@
 // This function 
+
+function removeElement(array, element) {
+    let index = array.indexOf(element);
+    if (index > -1) {
+        array.splice(index, 1);
+    }
+}
+
+function countElement(array, element) {
+    let counts = {};
+    for (var i = 0; i < array.length; i++) {
+        if (!counts.hasOwnProperty(array[i])) {
+            counts[array[i]] = 1;
+        }
+        else {
+            counts[array[i]]++;
+        }
+    }
+    return counts[element];
+}
+
+//-- user input
+
+// let guesses = ['A', 'B', 'C', 'C'];
+// let answer = 'OSCAR';
+
+let guesses = ['W', 'L', 'M', 'A', 'E', 'X'];
+let answer = 'MAXWELL';
+
+//---- don't change below
+
+let alphaList = ['A', 'B', 'C', 'D', 'E', 'F',
+    'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+    'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+let answers = answer.split("");
+
+console.log(alphaList);
+console.log(answers);
+console.log(guesses);
+
+console.log(" You have " + answer.length + " letters to guess.");
+
+// console.log("use function:" + countElement(answers, 'A'));
+
+
+
+// alogorithm
+for (let i = 0; i < guesses.length; i++) {
+    console.log("guessing " + guesses[i]);
+    if (answers.includes(guesses[i])) {
+        // console.log("right guess " + guesses[i]);
+        // console.log("count " + countElement(answers, guesses[i]));
+        let count = countElement(answers, guesses[i]);
+        for (let j = 0; j < count; j++) {
+            // console.log("right guess j " + j);
+            removeElement(answers, guesses[i]);
+        }
+    }
+    else {
+        console.log("wrong guess " + guesses[i]);
+    }
+    if (alphaList.includes(guesses[i])) {
+        removeElement(alphaList, guesses[i]);
+    }
+    else {
+        console.log("you already guess " + guesses[i] + ". Please choose a different letter. ");
+    }
+
+    console.log("Letters Left [ " + alphaList + " ]");
+
+
+}
+
+if (answers.length > 0) {
+    console.log("****** You Lose");
+    console.log("Missing letters: " + answers);
+    console.log("Correct Answer: " + answer);
+}
+else {
+    console.log("****** You Win");
+    console.log("You Guess the Correct Answer: " + answer);
+}
